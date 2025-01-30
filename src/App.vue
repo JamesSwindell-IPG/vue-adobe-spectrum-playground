53669ce470067835948e3b9d3d7def065f2a9d0e
James Swindell <james.swindell@ipghealth.com>
2025-01-30 10:40:48 -0500

    add adobe spectrum packages (core+button) and test button

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,10 @@
 
 <template>
   <h1>Vue Adobe Spectrum CSS Playground</h1>
+  <button
+    type="button"
+    class="spectrum-Button spectrum-Button--fill spectrum-Button--accent spectrum-Button--sizeM"
+  >
+    Click Me!
+  </button>
 </template>
